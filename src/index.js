1cf0f00f278532ef5c2999ab2889a3f05fb9b577
Faranosh  Amini <faranosh.amini4@gmail.com>
2022-10-01 12:53:27 +0430

    created index.js in src directory

--- src/index.js
+++ src/index.js
@@ -1,10 +1,42 @@
-function component() {
-  const element = document.createElement('div');
+import './modules/style.css';
 
-  // Lodash, currently included via a script, is required for this line to work
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+function showToDo(todo) {
+  const list = document.querySelector('.list');
+  const newlist = document.createElement('li');
+  newlist.classList.add('newlist');
+  newlist.innerHTML = `
+    <input id="${todo.index}" class="check" type="checkbox"/>
+    <span class="text">${todo.description}</span>
+    <button class="delete"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10.001 7.8a2.2 2.2 0 1 0 0 4.402A2.2 2.2 0 0 0 10 7.8zm0-2.6A2.2 2.2 0 1 0 9.999.8a2.2 2.2 0 0 0 .002 4.4zm0 9.6a2.2 2.2 0 1 0 0 4.402 2.2 2.2 0 0 0 0-4.402z"/></svg></button>
+    `;
+  list.append(newlist);
 }
 
-document.body.appendChild(component());
+const todoList = [];
+let id = 1;
+function add(text) {
+  const todo = {
+    description: text,
+    completed: false,
+    index: id += 1,
+  };
+  todoList.push(todo);
+  showToDo(todo);
+}
+
+const form = document.querySelector('.main');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const input = document.querySelector('.action');
+  const text = input.value.trim();
+  if (text !== '') {
+    add(text);
+    input.value = '';
+    input.focus();
+  }
+});
+
+const completed = document.querySelector('.newlist');
+completed.addEventListener('click', () => {
+  completed.classList.toggle('completed');
+});
